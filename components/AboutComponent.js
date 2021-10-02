2d42a6af2ccb44b8cdb7c0bc6ed7e4c4ca26dc2a
David Ta <david.ta555@gmail.com>
2021-10-02 11:38:24 -0400

    Week 1 Assignment Edit

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -4,23 +4,20 @@ import { Card, ListItem } from "react-native-elements";
 import { FlatList } from "react-native-gesture-handler";
 import { PARTNERS } from "../shared/partners";
 
-function Misson(props) {
-  if (props) {
-    return (
-      <Card title="Our Misson">
-        <Text style={{ margin: 10 }}>
-          We present a curated database of the best campsites in the vast woods
-          and backcountry of the World Wide Web Wilderness. We increase access
-          to adventure for the public while promoting safe and respectful use of
-          resources. The expert wilderness trekkers on our staff personally
-          verify each campsite to make sure that they are up to our standards.
-          We also present a platform for campers to share reviews on campsites
-          they have visited with each other.
-        </Text>
-      </Card>
-    );
-  }
-  return <View />;
+function Misson() {
+  return (
+    <Card title="Our Misson">
+      <Text style={{ margin: 10 }}>
+        We present a curated database of the best campsites in the vast woods
+        and backcountry of the World Wide Web Wilderness. We increase access to
+        adventure for the public while promoting safe and respectful use of
+        resources. The expert wilderness trekkers on our staff personally verify
+        each campsite to make sure that they are up to our standards. We also
+        present a platform for campers to share reviews on campsites they have
+        visited with each other.
+      </Text>
+    </Card>
+  );
 }
 
 class About extends Component {
